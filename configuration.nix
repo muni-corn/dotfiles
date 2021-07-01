a34644bc0c4e6674530223ec3152cdee5115dcdf
municorn <municorn@musicaloft.com>
2021-07-01 09:36:56 -0600

    Add docker

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,12 @@
     pinentry-curses
   ];
 
+  virtualisation.docker = {
+    enable = true;
+    extraOptions = "--data-root /home/docker/";
+    autoPrune.enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.sway.enable = true;
