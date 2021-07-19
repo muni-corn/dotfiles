c8ed8f0c6bf38634cdf36b63cf44859903224b90
municorn <municorn@musicaloft.com>
2021-07-18 22:30:39 -0600

    Add laptop hardware config

--- laptop-configuration.nix
+++ laptop-configuration.nix
@@ -8,7 +8,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./laptop-hardware.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
