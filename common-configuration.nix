af4eb4a3e24492f8d7c484e8591c0d749bae81a9
municorn <municorn@musicaloft.com>
2021-07-25 19:19:33 -0600

    Add xdg

--- common-configuration.nix
+++ common-configuration.nix
@@ -203,5 +203,15 @@
     autoPrune.enable = true;
   };
 
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+      gtkUsePortal = true;
+    };
+  };
 }
 
