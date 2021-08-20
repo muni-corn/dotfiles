e82443ce75bc114e314b46ecb4ec151b828a7933
municorn <municorn@musicaloft.com>
2021-08-19 22:44:40 -0600

    Modify toggle_gammastep.fish
    - Use a lock file to prevent repetitive runs
    - Add  for easier customization

--- homes/muni/sway/scripts/toggle_gammastep.fish
+++ homes/muni/sway/scripts/toggle_gammastep.fish
@@ -1,9 +1,24 @@
 #! /usr/bin/env fish
 
-if pgrep -x gammastep > /dev/null
+set -l lock_file_path $HOME/.toggle_gammastep.lock
+
+# create a lock file if it doesn't exist already.
+# if the lock file exists, it's likely that the script is already running
+if ! echo >? $lock_file_path ^/dev/null
+    echo "this script is busy"
+    exit 1
+end
+
+# remove lock on exit
+trap "rm -f $lock_file_path" EXIT
+
+set -l feature_name "Night Display"
+
+if systemctl --user --quiet is-active gammastep
+    set nid (notify-desktop -u low "Turning $feature_name off" "Turn $feature_name on before bed to help you sleep better.")
     systemctl --user stop gammastep
-    and notify-send -u low -t 5000 "Night Display is now off" "Turn Night Display on before bed to help you sleep better."
+    and notify-desktop -u low -t 5000 -r $nid "$feature_name is now off" "Turn $feature_name on before bed to help you sleep better."
 else
     systemctl --user start gammastep
-    and notify-send -u low -t 5000 "Night Display is now on" "Night Display filters blue light to help you sleep when it's late."
+    and notify-desktop -u low -t 5000 "$feature_name is now on" "$feature_name filters blue light to help you sleep when it's late."
 end
