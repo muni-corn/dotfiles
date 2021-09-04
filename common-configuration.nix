2b49b4950e3527cc8f5745ec26895bd0b9476d25
municorn <municorn@musicaloft.com>
2021-09-04 00:53:20 -0500

    Revert "Enable auto-upgrade"
    This reverts commit 8b6ea93fee70f99a38e425fc00871197f78e0e97...
    
    ...and was also inevitable.

--- common-configuration.nix
+++ common-configuration.nix
@@ -188,16 +188,6 @@
     };
   };
 
-  system = {
-    autoUpgrade = {
-      enable = true;
-      allowReboot = true;
-      flags = [ "--update-input" "nixpkgs" "--commit-lock-file" ];
-      flake = "/etc/nixos";
-      dates = "03:00";
-    };
-  };
-
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
