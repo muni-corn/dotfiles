80d3c95a21be7d5e0dd64a68195f443d93c7d8ac
municorn <municorn@musicaloft.com>
2021-10-20 10:56:07 -0500

    sway/scripts: Use `dunstify` instead of `notify-desktop`

--- homes/muni/sway/scripts/toggle_gammastep.fish
+++ homes/muni/sway/scripts/toggle_gammastep.fish
@@ -15,10 +15,10 @@ trap "rm -f $lock_file_path" EXIT
 set -l feature_name "Night Display"
 
 if systemctl --user --quiet is-active gammastep
-    set nid (notify-desktop -u low "Turning $feature_name off" "Turn $feature_name on before bed to help you sleep better.")
+    set nid (dunstify -p -u low "Turning $feature_name off" "Turn $feature_name on before bed to help you sleep better.")
     systemctl --user stop gammastep
-    and notify-desktop -u low -t 5000 -r $nid "$feature_name is now off" "Turn $feature_name on before bed to help you sleep better."
+    and dunstify -u low -t 5000 -r $nid "$feature_name is now off" "Turn $feature_name on before bed to help you sleep better."
 else
     systemctl --user start gammastep
-    and notify-desktop -u low -t 5000 "$feature_name is now on" "$feature_name filters blue light to help you sleep when it's late."
+    and dunstify -u low -t 5000 "$feature_name is now on" "$feature_name filters blue light to help you sleep when it's late."
 end
