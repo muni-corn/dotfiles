479ad15c474ce689b81d20210636615324dcd92a
municorn <municorn@musicaloft.com>
2021-10-23 00:51:06 -0500

    starship: Change format

--- homes/muni/starship.nix
+++ homes/muni/starship.nix
@@ -2,8 +2,16 @@
 {
   enable = true;
   settings = {
+    format = ''$all$shell$jobs$status$character'';
+    battery = {
+      display.threshold = 30;
+    };
     package = {
       disabled = true;
+    };
+    status = {
+      disabled = false;
+      symbol = "•";
     };
   };
 }
