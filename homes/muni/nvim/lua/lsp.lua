ccf07c6b4679153b2d5688dd9374c2cff85e073a
municorn <municorn@musicaloft.com>
2021-10-26 23:21:36 -0500

    nvim/lsp: Improve `<c-n>` mapping

--- homes/muni/nvim/lua/lsp.lua
+++ homes/muni/nvim/lua/lsp.lua
@@ -32,9 +32,15 @@ cmp.setup {
         minheight = 1,
     };
     mapping = {
-        ['<c-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ['<c-u>'] = cmp.mapping.scroll_docs(-3),
         ['<c-d>'] = cmp.mapping.scroll_docs(3),
+        ['<c-n>'] = function(fallback)
+            if cmp.visible() then
+                cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+            else
+                cmp.mapping.complete()
+            end
+        end,
         ['<c-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ['<c-l>'] = cmp.mapping.confirm({ select = true, behavior = cmp.SelectBehavior.Replace }),
         ['<c-q>'] = cmp.mapping.close(),
