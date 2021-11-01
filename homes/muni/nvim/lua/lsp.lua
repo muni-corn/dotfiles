f568541d85a052e38a3ffc6d494884c35c57416f
municorn <municorn@musicaloft.com>
2021-10-31 21:50:58 -0500

    nvim/lsp: Improve capabilities

--- homes/muni/nvim/lua/lsp.lua
+++ homes/muni/nvim/lua/lsp.lua
@@ -62,7 +62,7 @@ cmp.setup {
 
 -- Enable language servers
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').update_capabilities(lsp_status.capabilities)
 -- capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- capabilities.textDocument.completion.completionItem.resolveSupport = {
 --   properties = {
