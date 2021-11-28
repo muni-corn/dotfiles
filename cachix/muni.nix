2f9bd9cef26c2a29db6e29c3a6de0a7db67e5b72
municorn <municorn@musicaloft.com>
2021-11-28 12:02:14 -0600

    cachix: Add me :)

---
+++ cachix/muni.nix
@@ -0,0 +1,11 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://municorn.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "municorn.cachix.org-1:hJDxYy9Z6ZYp7SEHK5tyhfQgjAIzXOA1WG4Kr18iPUA="
+    ];
+  };
+}
