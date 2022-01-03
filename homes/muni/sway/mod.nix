8ecc384ea8c689621c2c84ed6245760ec08b7c11
municorn <municorn@musicaloft.com>
2022-01-03 16:31:22 -0600

    sway: Remove trailing whitespace

--- homes/muni/sway/mod.nix
+++ homes/muni/sway/mod.nix
@@ -192,8 +192,8 @@ in
     export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
     export SDL_VIDEODRIVER=wayland
     export _JAVA_AWT_WM_NONREPARENTING=1
-    export XDG_CURRENT_DESKTOP=sway 
-    export XDG_SESSION_TYPE=wayland 
+    export XDG_CURRENT_DESKTOP=sway
+    export XDG_SESSION_TYPE=wayland
   '';
 
   systemdIntegration = true;
