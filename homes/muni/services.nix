6ca62af144265f161639aa7fa0d2fd7084df93eb
municorn <municorn@musicaloft.com>
2022-02-22 16:24:05 -0600

    swayidle: Unquote commands
    the module *should* handle this by itself

--- homes/muni/services.nix
+++ homes/muni/services.nix
@@ -29,9 +29,9 @@
     ];
     timeouts =
       let
-        lockWarningCmd = '''notify-send -u low -t 29500 -- "Are you still there?" "Your system will lock itself soon."' '';
-        dpmsOff = '''swaymsg "output * dpms off"' '';
-        dpmsOn = '''swaymsg "output * dpms on" '';
+        lockWarningCmd = "notify-send -u low -t 29500 -- 'Are you still there?' 'Your system will lock itself soon.'";
+        dpmsOff = "swaymsg 'output * dpms off'";
+        dpmsOn = "swaymsg 'output * dpms on'";
       in
       [
         { timeout = 570; command = lockWarningCmd; }
