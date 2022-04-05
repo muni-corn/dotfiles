71db7dac07fc56d07c26432c299e554607506f8a
municorn <municorn@musicaloft.com>
2022-04-04 21:24:58 -0500

    capture/dslr_stream: Remove threads option

--- homes/muni/sway/scripts/capture/dslr_stream.fish
+++ homes/muni/sway/scripts/capture/dslr_stream.fish
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 2 -f v4l2 /dev/video0
+gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -f v4l2 /dev/video0
