7799d715e11eef745cc22c407de653e59b6f88d7
municorn <municorn@musicaloft.com>
2022-05-23 00:39:50 -0500

    sway/emoji_menu: Trim newline with `wl-copy`

--- homes/muni/sway/scripts/emoji_menu.fish
+++ homes/muni/sway/scripts/emoji_menu.fish
@@ -1,3 +1,3 @@
 #! /usr/bin/env fish
 
-cat $HOME/.emoji_list.txt | bemenu -p Emoji $argv | string split -m1 -f1 ' ' | wl-copy
+cat $HOME/.emoji_list.txt | bemenu -p Emoji $argv | string split -m1 -f1 ' ' | wl-copy --trim-newline
