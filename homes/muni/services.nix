937b1798e26115412e5906fb872a2bd3770703aa
municorn <municorn@musicaloft.com>
2022-06-02 22:01:11 -0500

    services: Enable gnupg ssh cache

--- homes/muni/services.nix
+++ homes/muni/services.nix
@@ -19,8 +19,11 @@
     enable = true;
 
     defaultCacheTtl = 86400;
-    enableSshSupport = true;
     maxCacheTtl = 86400;
+
+    enableSshSupport = true;
+    defaultCacheTtlSsh = 86400;
+    maxCacheTtlSsh = 86400;
     sshKeys = [ "23BF04AE05B5DAC1267FE74CD9F1DB7D2367AAE8" ];
 
     extraConfig = "no-allow-external-cache";
