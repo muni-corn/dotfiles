2aa29cede3c658b8efcc72572926a40f001b323a
municorn <municorn@musicaloft.com>
2022-06-28 21:32:00 -0500

    muse/palette: Update palette and swatch types

--- homes/muni/muse/palette.nix
+++ homes/muni/muse/palette.nix
@@ -1,6 +1,8 @@
 { lib }:
 
 let
+  inherit (lib) types;
+
   defaultSwatch = bases:
     with bases;
     {
@@ -46,19 +48,60 @@ let
       if (bases ? swatch) then (lib.attrsets.recursiveUpdate default bases.swatch) else default;
   };
 
-  swatchType = lib.mkOptionType {
-    name = "swatch";
-    description = "A set of named colors for convenience when working with a base16 palette.";
+  mkColorBaseOption = name: lib.mkOption {
+    type = types.strMatching "^[A-Fa-f0-9]{6}$";
+    description = "The ${name} color.";
+    visible = false;
+  };
+
+  optionsFromNames = names: builtins.listToAttrs (map (name: { inherit name; value = mkColorBaseOption name; }) names);
+
+  swatchType = types.submodule {
+    options = optionsFromNames [
+      "background"
+      "black"
+      "gray"
+
+      "foreground"
+      "white"
+      "silver"
+
+      "accent"
+      "warning"
+      "alert"
+    ];
   };
 in
 {
   inherit mkSwatch swatchType;
 
-  paletteType = lib.mkOptionType {
-    name = "base16Palette";
-    description = "A palette of 16 colors: 8 monochrome shades and 8 colors.";
-    nestedTypes = {
-      inherit swatchType;
+  paletteType =
+    let
+      baseNames = [
+        "base00"
+        "base01"
+        "base02"
+        "base03"
+        "base04"
+        "base05"
+        "base06"
+        "base07"
+        "base08"
+        "base09"
+        "base0A"
+        "base0B"
+        "base0C"
+        "base0D"
+        "base0E"
+        "base0F"
+      ];
+    in
+    types.submodule {
+      options = (optionsFromNames baseNames) // {
+        swatch = lib.mkOption {
+          type = swatchType;
+          description = "A swatch with named colors for convenience.";
+        };
+      };
     };
-  };
 }
