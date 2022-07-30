7b0d7a27de8988e0a896f9383e5c61e211a0b4da
municorn <municorn@musicaloft.com>
2022-07-29 23:04:43 -0500

    quick-code-script: Generate dir list from zoxide db

--- homes/muni/quick-code-script.nix
+++ homes/muni/quick-code-script.nix
@@ -1,23 +1,10 @@
 { bemenuArgsJoined, config, pkgs }:
 
-let
-  dirs = [
-    "~/.config/nixpkgs"
-    "~/code/blockery"
-    "~/code/chicky-chicky"
-    "~/code/matchpal"
-    "~/code/muse-status"
-    "~/code/silverfox"
-    "~/notebook"
-  ];
-
-  dirsListFile = pkgs.writeText "quick-code-dir-list" (pkgs.lib.concatStringsSep "\n" dirs);
-in
 pkgs.writeScript "quick-code-script"
   ''
     #!${pkgs.fish}/bin/fish
 
-    set dir (cat ${dirsListFile} | ${pkgs.bemenu}/bin/bemenu -p "Where to?" ${bemenuArgsJoined} || exit 1)
+    set dir (${config.programs.zoxide.package}/bin/zoxide query -l | string replace "${config.home.homeDirectory}" "~" | ${pkgs.bemenu}/bin/bemenu -p "Where to?" ${bemenuArgsJoined} || exit 1)
 
     ${pkgs.kitty}/bin/kitty -d $dir -e ${pkgs.fish}/bin/fish -i
   ''
