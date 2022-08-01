507cfa04fc394c32dbc0d2a0482a66b092bbeb68
municorn <municorn@musicaloft.com>
2022-07-31 23:03:06 -0500

    quick-code-script: Add documentation
    haha

--- homes/muni/quick-code-script.nix
+++ homes/muni/quick-code-script.nix
@@ -1,3 +1,10 @@
+/*
+  Simplifies and quickens the process of opening a terminal and using `z` (zoxide)
+  to navigate to a directory, a *super* common first couple of steps when doing
+  anythign in the terminal. This speeds it up by using `bemenu` to pick from a
+  list of directories in the zoxide database. (And directories unknown to zoxide
+  should be allowed too; they will be added to zoxide if so)
+*/
 { bemenuArgsJoined, config, pkgs }:
 
 pkgs.writeScript "quick-code-script"
