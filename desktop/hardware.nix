da6a9e24d3548b10e9b701236f6dcf87b824f73c
municorn <municorn@musicaloft.com>
2022-08-08 22:42:01 -0500

    desktop/hardware: Some formatting

--- desktop/hardware.nix
+++ desktop/hardware.nix
@@ -64,9 +64,13 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = lib.mkDefault false;
-  networking.interfaces.enp34s0.useDHCP = lib.mkDefault true;
-  networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
+  networking = {
+    useDHCP = lib.mkDefault false;
+    interfaces = {
+      enp34s0.useDHCP = lib.mkDefault true;
+      wlan0.useDHCP = lib.mkDefault true;
+    };
+  };
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
