0c0586f42265842e09dc9f6b18b91be1fcee588c
municorn <municorn@musicaloft.com>
2022-08-09 17:12:47 -0500

    desktop/btrbk: Adjust preservation policies
    i think that snapshots (on the same volume) should be useful for more
    recent backups/restores, while backups on a "cold storage" volume
    (separate from the main volume) should last longer. this keeps the main
    volume somewhat clean and frees up space more, while we still have a
    good amount of backups to restore from on the other disk we have.

--- desktop/btrbk.nix
+++ desktop/btrbk.nix
@@ -12,12 +12,12 @@
 
         snapshot_create = "onchange";
         snapshot_dir = "/snaps";
-        snapshot_preserve_min = "3h";
-        snapshot_preserve = "24h 7d 5w 12m 3y";
+        snapshot_preserve_min = "12h";
+        snapshot_preserve = "24h 7d 4w 2m";
 
         target = "/backup/ponycastle";
-        target_preserve = "24h 7d 8w 12m 2y";
-        target_preserve_min = "3h";
+        target_preserve = "24h 7d 8w 12m 1y";
+        target_preserve_min = "1h";
       };
     };
   };
