ff3fdc331b0c153ef77f8ee009b952e5c1db3a3f
municorn <municorn@musicaloft.com>
2023-02-03 23:28:05 -0600

    Update spotifyd config

--- homes/muni/spotifyd.nix
+++ homes/muni/spotifyd.nix
@@ -11,9 +11,9 @@
   };
   settings = {
     global = {
-      username = "municorn.-us";
-      password_cmd = "${config.programs.password-store.package}/bin/pass spotify.com/municorn.-us";
-      device_name = deviceInfo.name;
+      username = "314nfayodu4tyk5lynekgqre3wty";
+      password_cmd = "${config.programs.password-store.package}/bin/pass spotify.com/municorn@musicaloft.com";
+      device_name = "${deviceInfo.name}-spotifyd";
       device_type = "computer";
       use_keyring = false;
       use_mpris = true;
