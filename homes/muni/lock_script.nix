5139798a4edefe115f5e521ba1aa81d1c9f31396
municorn <municorn@musicaloft.com>
2023-03-14 09:09:26 -0500

    home: Fix lock script

--- homes/muni/lock_script.nix
+++ homes/muni/lock_script.nix
@@ -3,14 +3,7 @@
   pkgs,
   ...
 }: let
-  colors = config.muse.theme.finalPalette;
-
-  bg = colors.swatch.background;
-  fg = colors.swatch.foreground + "c0";
-  fg_faded = colors.swatch.foreground + "80";
-  primary = colors.swatch.accent + "80";
-  warning = colors.swatch.warning;
-  error = colors.swatch.alert;
+  bg = config.muse.theme.finalPalette.swatch.background;
 in
   pkgs.writeScript "lock-script"
   ''
@@ -33,8 +26,7 @@ in
 
     ${pkgs.pipewire}/bin/pw-play "${pkgs.muse-sounds}/share/sounds/musicaloft/stereo/Lock.oga" &
 
-    set --local transparent "00000000"
-    set --local image_args ""
+    set --local image_args
     set --local images
 
     # take screenshot of each output and blur it
@@ -43,49 +35,11 @@ in
         ${pkgs.grim}/bin/grim -o $output $image_file
         ${pkgs.imagemagick}/bin/convert "$image_file" -resize 5% -fill "#${bg}" -colorize 25% -blur 15x1 -resize 2000% "$image_file"
         # TODO: use `composite` to overlay a lock icon
-        set image_args $image_args "--image" "$output:$image_file"
+        set image_args $image_args "-i" "$output:$image_file"
         set images $images $image_file
     end
 
-    ${pkgs.swaylock}/bin/swaylock $fork_arg \
-        $image_args \
-    \
-        --ignore-empty-password \
-        --scaling=fill \
-        --disable-caps-lock-text \
-        --indicator-caps-lock \
-        --color 000000 \
-    \
-        --ring-color=$transparent \
-        --ring-wrong-color=${error} \
-        --ring-ver-color=$transparent \
-        --ring-caps-lock-color=$transparent \
-        --ring-clear-color=${warning} \
-    \
-        --indicator-radius 128 \
-        --indicator-thickness 4 \
-    \
-        --inside-color=$transparent \
-        --inside-ver-color=$transparent \
-        --inside-wrong-color=$transparent \
-        --inside-clear-color=$transparent \
-        --inside-caps-lock-color=$transparent \
-    \
-        --key-hl-color=${fg_faded} \
-        --bs-hl-color=${primary} \
-        --caps-lock-key-hl-color=${warning} \
-        --caps-lock-bs-hl-color=${primary} \
-        --separator-color=$transparent \
-        -n \
-    \
-        --text-color=${fg} \
-        --text-ver-color=$transparent \
-        --text-clear-color=$transparent \
-        --text-wrong-color=$transparent \
-        --text-caps-lock-color=${warning} \
-    \
-        --font "sans Thin" \
-        --font-size 12 \
+    ${pkgs.swaylock}/bin/swaylock $fork_arg $image_args
 
     rm $images
   ''
