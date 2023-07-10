778fb4ccfb02a7257725cef99af4f7b94b17486f
municorn <municorn@musicaloft.com>
2023-07-10 12:44:23 -0500

    sway/scripts: Fix kaomoji copying issue

--- homes/muni/sway/scripts/emoji_menu.fish
+++ homes/muni/sway/scripts/emoji_menu.fish
@@ -1,3 +1,3 @@
 #! /usr/bin/env fish
 
-cat $HOME/.emoji_list.txt | bemenu -p Emoji $argv | string split -m1 -f1 ' ' | wl-copy --trim-newline
+cat $HOME/.emoji_list.txt | bemenu -p Emoji $argv | string split -m1 -f1 -r ' (' | wl-copy --trim-newline
