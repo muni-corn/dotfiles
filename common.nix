ad158d2728fc83ecd463cac5eccd4cad8c072d17
municorn <municorn@musicaloft.com>
2023-07-29 07:54:56 -0500

    common: Formatting and remove explicit default option

--- common.nix
+++ common.nix
@@ -276,11 +276,12 @@
 
     pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
       pulse.enable = true;
       jack.enable = true;
-      wireplumber.enable = true;
     };
 
     postgresql = {
