04780851775c0f5357604021190bb0b8fd1ac5d0
municorn <municorn@musicaloft.com>
2023-08-10 21:27:54 -0500

    common: Remove wlr xdg portal, add gtk portal
    gtk portal fixes issues with gtk styling in hyprland without conflicting
    with xdph

--- common.nix
+++ common.nix
@@ -382,7 +382,9 @@
   xdg = {
     portal = {
       enable = true;
-      wlr.enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
     };
     sounds.enable = true;
   };
