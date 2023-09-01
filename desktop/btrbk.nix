a7f37b5cf5f7c37a66a9d693fb82518508cdb2d2
municorn <municorn@musicaloft.com>
2023-09-01 11:09:20 -0500

    desktop/btrbk: Add backup support for archive drive

--- desktop/btrbk.nix
+++ desktop/btrbk.nix
@@ -7,17 +7,19 @@
   services.btrbk.instances.backup = {
     onCalendar = "*:00/15";
     settings = {
-      volume."/" = {
-        subvolume = "home";
-
-        snapshot_create = "onchange";
-        snapshot_dir = "/snaps";
-        snapshot_preserve_min = "12h";
-        snapshot_preserve = "24h 7d 4w 2m";
-
-        target = "/backup/ponycastle";
-        target_preserve = "24h 7d 8w 12m 1y";
-        target_preserve_min = "1h";
+      snapshot_create = "onchange";
+      snapshot_preserve_min = "12h";
+      snapshot_preserve = "24h 7d 4w 2m";
+      target_preserve = "24h 7d 8w 12m 1y";
+      target_preserve_min = "1h";
+      preserve_hour_of_day = "5";
+      volume = {
+        "/" = {
+          subvolume = "home";
+          snapshot_dir = "/snaps";
+          target = "/backup/ponycastle";
+        };
+        "/archive".target = "/backup/archive";
       };
     };
   };
