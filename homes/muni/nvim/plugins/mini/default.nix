7314ea098b640cffe99e8d018fe840fe5600ac22
municorn <municorn@musicaloft.com>
2023-10-12 09:01:48 -0500

    feat(nvim): enable mini.basics

--- homes/muni/nvim/plugins/mini/default.nix
+++ homes/muni/nvim/plugins/mini/default.nix
@@ -8,6 +8,12 @@
     enable = true;
     modules = {
       animate = {__empty = null;};
+      basics = {
+        options = {
+          extra_ui = true;
+          win_borders = "rounded";
+        };
+      };
       comment = {__empty = null;};
       indentscope = {__empty = null;};
       sessions = {__empty = null;};
