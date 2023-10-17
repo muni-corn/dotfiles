1da58b3a5d66408aa90123bd48ecdac25c96dc35
municorn <municorn@musicaloft.com>
2023-10-16 21:45:17 -0500

    feat(spotifyd): make into usable nix module

--- muni/spotifyd.nix
+++ muni/spotifyd.nix
@@ -1,23 +1,27 @@
 {
   config,
-  deviceName,
   pkgs,
+  osConfig,
   ...
-}: {
-  enable = true;
-  package = pkgs.spotifyd.override {
-    withPulseAudio = true;
-    withMpris = true;
-  };
-  settings = {
-    global = {
-      username = "314nfayodu4tyk5lynekgqre3wty";
-      password_cmd = "${config.programs.password-store.package}/bin/pass spotify.com/municorn@musicaloft.com";
-      device_name = "${deviceName}-spotifyd";
-      device_type = "computer";
-      use_keyring = false;
-      use_mpris = true;
-      backend = "pulseaudio";
+}: let
+  deviceName = osConfig.networking.hostName;
+in {
+  services.spotifyd = {
+    enable = true;
+    package = pkgs.spotifyd.override {
+      withPulseAudio = true;
+      withMpris = true;
+    };
+    settings = {
+      global = {
+        username = "314nfayodu4tyk5lynekgqre3wty";
+        password_cmd = "${config.programs.password-store.package}/bin/pass spotify.com/municorn@musicaloft.com";
+        device_name = "${deviceName}-spotifyd";
+        device_type = "computer";
+        use_keyring = false;
+        use_mpris = true;
+        backend = "pulseaudio";
+      };
     };
   };
 }
