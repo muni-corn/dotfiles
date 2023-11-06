8d8095071a3ce931bddac3bb44d63ecc899a7330
municorn <municorn@musicaloft.com>
2023-11-06 08:27:43 -0600

    feat(programs): configure htop to be prettier and more useful

--- muni/programs.nix
+++ muni/programs.nix
@@ -48,61 +48,57 @@
       enable = true;
       settings = let
         inherit (config.lib.htop) fields text bar leftMeters rightMeters;
-      in
-        {
-          fields = with fields; [
-            PID
-            USER
-            PRIORITY
-            NICE
-            M_SIZE
-            M_RESIDENT
-            M_SHARE
-            STATE
-            PERCENT_CPU
-            PERCENT_MEM
-            TIME
-            COMM
-          ];
-          hide_kernel_threads = 1;
-          hide_userland_threads = 0;
-          shadow_other_users = 0;
-          show_thread_names = 1;
-          show_program_path = 0;
-          highlight_base_name = 1;
-          highlight_megabytes = 1;
-          highlight_threads = 1;
-          highlight_changes = 0;
-          highlight_changes_delay_secs = 5;
-          find_comm_in_cmdline = 1;
-          strip_exe_from_cmdline = 1;
-          show_merged_command = 0;
-          tree_view = 0;
-          tree_view_always_by_pid = 0;
-          header_margin = 1;
-          detailed_cpu_time = 0;
-          cpu_count_from_one = 1;
-          show_cpu_usage = 1;
-          show_cpu_frequency = 0;
-          show_cpu_temperature = 0;
-          degree_fahrenheit = 1;
-          update_process_names = 0;
-          account_guest_in_cpu_meter = 0;
-          color_scheme = 5;
-          enable_mouse = 1;
-          delay = 50;
-          hide_function_bar = 0;
-        }
-        // (leftMeters [
-          (bar "AllCPUs")
-          (bar "Memory")
-          (bar "Swap")
-        ])
-        // (rightMeters [
-          (text "Tasks")
-          (text "LoadAverage")
-          (text "Uptime")
-        ]);
+      in {
+        fields = with fields; [
+          PID
+          USER
+          PRIORITY
+          NICE
+          M_SIZE
+          M_RESIDENT
+          M_SHARE
+          STATE
+          PERCENT_CPU
+          PERCENT_MEM
+          TIME
+          COMM
+        ];
+        hide_kernel_threads = 1;
+        hide_userland_threads = 0;
+        shadow_other_users = 0;
+        show_thread_names = 1;
+        show_program_path = 0;
+        highlight_base_name = 1;
+        highlight_megabytes = 1;
+        highlight_threads = 1;
+        highlight_changes = 0;
+        highlight_changes_delay_secs = 5;
+        find_comm_in_cmdline = 1;
+        strip_exe_from_cmdline = 1;
+        show_merged_command = 0;
+        tree_view = 0;
+        tree_view_always_by_pid = 0;
+        header_margin = 1;
+        detailed_cpu_time = 0;
+        cpu_count_from_one = 1;
+        show_cpu_usage = 1;
+        show_cpu_frequency = 0;
+        show_cpu_temperature = 0;
+        degree_fahrenheit = 1;
+        update_process_names = 0;
+        account_guest_in_cpu_meter = 0;
+        enable_mouse = 1;
+        delay = 50;
+        hide_function_bar = 0;
+
+        header_layout = "three_33_34_33";
+        column_meters_0 = "LeftCPUs2 Memory DiskIO";
+        column_meter_modes_0 = "1 1 2";
+        column_meters_1 = "RightCPUs2 Swap NetworkIO";
+        column_meter_modes_1 = "1 1 2";
+        column_meters_2 = "Clock Date Uptime Tasks LoadAverage System";
+        column_meter_modes_2 = "4 2 2 2 2 2";
+      };
     };
 
     imv.enable = true;
