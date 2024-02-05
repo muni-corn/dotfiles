26a8e3d2945bec7cc9fb7c94e19dfcfaa93b928d
municorn <municorn@musicaloft.com>
2024-02-05 16:59:22 -0600

    refactor(nvim): make warnings orange, refactor links

--- muni/nvim/highlight.nix
+++ muni/nvim/highlight.nix
@@ -3,6 +3,9 @@
   lib,
   ...
 }: let
+  inherit (builtins) listToAttrs map;
+  inherit (lib.attrsets) concatMapAttrs nameValuePair;
+
   nameToCterm = {
     black = "Black";
     dark-gray = "DarkGray";
@@ -57,173 +60,197 @@
     }
     // attrs;
   link = to: {link = to;};
-in {
-  programs.nixvim.highlight = {
-    "@neorg.headings.1.prefix" = hl "brown" null {};
-    "@neorg.headings.1.title" = hl "brown" null {};
-    "@neorg.markup.verbatim" = hl "orange" null {};
-    "@text.literal" = hl "green" null {};
-    BarPill = hl "dark-gray" null {};
-    Bold = hl null null {bold = true;};
-    Boolean = hl "orange" null {};
-    Character = hl "red" null {};
-    CmpItemAbbr = hl "light-gray" null {};
-    CmpItemAbbrMatch = hl "cyan" null {bold = true;};
-    CmpItemAbbrMatchFuzzy = hl "blue" null {};
-    CmpItemKind = hl "purple" null {};
-    CmpItemMenu = hl "gray" null {};
-    ColorColumn = hl null "dark-gray" {};
-    Comment = hl "light-gray" null {};
-    Conceal = hl "gray" null {};
-    Conditional = hl "brown" null {};
-    Constant = hl "orange" null {};
-    Cursor = hl "black" "light-silver" {};
-    CursorColumn = hl null "dark-gray" {};
-    CursorLine = hl null "black" {};
-    CursorLineNr = hl "silver" null {bold = true;};
-    CustomBlueStatus = hl "blue" "dark-gray" {bold = true;};
-    CustomCyanStatus = hl "cyan" "dark-gray" {bold = true;};
-    CustomFuchsiaStatus = hl "purple" "dark-gray" {bold = true;};
-    CustomLimeStatus = hl "green" "dark-gray" {bold = true;};
-    CustomRedStatus = hl "red" "dark-gray" {bold = true;};
-    CustomYellowStatus = hl "yellow" "dark-gray" {bold = true;};
-    Debug = hl "red" null {};
-    Define = hl "purple" null {};
-    Delimiter = hl "light-silver" null {};
-    DiagnosticError = hl "red" null {};
-    DiagnosticHint = hl "blue" null {};
-    DiagnosticInfo = hl "cyan" null {};
-    DiagnosticOk = hl "green" null {};
-    DiagnosticWarn = hl "yellow" null {};
-    DiagnosticUnderlineError = hl null null {
-      undercurl = true;
-      sp = nameToHex "red";
-    };
-    DiagnosticUnderlineHint = hl null null {
-      undercurl = true;
-      sp = nameToHex "blue";
-    };
-    DiagnosticUnderlineInfo = hl null null {
-      undercurl = true;
-      sp = nameToHex "cyan";
-    };
-    DiagnosticUnderlineOk = hl null null {
-      underline = true;
-      sp = nameToHex "green";
-    };
-    DiagnosticUnderlineWarn = hl null null {
-      undercurl = true;
-      sp = nameToHex "yellow";
-    };
-    DiffAdd = hl "green" "dark-gray" {bold = true;};
-    DiffChange = hl null null {};
-    DiffDelete = hl "red" "dark-gray" {bold = true;};
-    DiffText = hl "yellow" "dark-gray" {bold = true;};
-    Directory = hl "blue" null {};
-    Error = hl "red" null {bold = true;};
-    Exception = hl "red" null {};
-    Float = hl "orange" null {};
-    FloatBorder = hl "gray" null {};
-    FoldColumn = hl "cyan" null {};
-    Folded = hl "gray" "black" {};
-    Function = hl "blue" null {};
-    GitGutterAdd = hl "green" null {bold = true;};
-    GitGutterChange = hl "orange" null {bold = true;};
-    GitGutterDelete = hl "red" null {bold = true;};
-    Identifier = hl "red" null {};
-    IncSearch = hl "black" "yellow" {};
-    Include = hl "blue" null {};
-    Info = hl "cyan" null {bold = true;};
-    Italic = hl null null {italic = true;};
-    Keyword = hl "purple" null {};
-    Label = hl "yellow" null {};
-    LineNr = hl "light-gray" null {};
-    Macro = hl "red" null {};
-    MatchParen = hl "bright-white" "dark-gray" {bold = true;};
-    ModeMsg = hl "green" null {};
-    MoreMsg = hl "green" null {};
-    NonText = hl "light-gray" null {};
-    Normal = hl "light-silver" null {};
-    NotifyBackground = hl "dark-gray" null {};
-    NotifyDEBUGBorder = hl "green" null {};
-    NotifyDEBUGIcon = hl "green" null {};
-    NotifyDEBUGTitle = hl "green" null {bold = true;};
-    NotifyERRORBorder = hl "red" null {};
-    NotifyERRORIcon = hl "red" null {};
-    NotifyERRORTitle = hl "red" null {bold = true;};
-    NotifyINFOBorder = hl "cyan" null {};
-    NotifyINFOIcon = hl "cyan" null {};
-    NotifyINFOTitle = hl "cyan" null {bold = true;};
-    NotifyTRACEBorder = hl "purple" null {};
-    NotifyTRACEIcon = hl "purple" null {};
-    NotifyTRACETitle = hl "purple" null {bold = true;};
-    NotifyWARNBorder = hl "yellow" null {};
-    NotifyWARNIcon = hl "yellow" null {};
-    NotifyWARNTitle = hl "yellow" null {bold = true;};
-    Number = hl "orange" null {};
-    Operator = hl "light-silver" null {};
-    Pmenu = hl "light-silver" "dark-gray" {};
-    PmenuSbar = hl null null {};
-    PmenuSel = hl "dark-gray" "light-silver" {bold = true;};
-    PmenuThumb = hl "light-silver" "light-silver" {};
-    PreProc = hl "yellow" null {};
-    Question = hl "blue" null {};
-    QuickFixLine = hl null "dark-gray" {};
-    Repeat = hl "yellow" null {};
-    Search = hl "dark-gray" "orange" {};
-    SignColumn = hl null null {};
-    Special = hl "cyan" null {};
-    SpecialChar = hl "brown" null {};
-    SpecialKey = hl "light-gray" null {};
-    SpellBad = hl "red" null {undercurl = true;};
-    SpellCap = hl "purple" null {};
-    SpellRare = hl "yellow" null {undercurl = true;};
-    Statement = hl "red" null {};
-    StatusLine = hl "light-gray" "dark-gray" {italic = true;};
-    StatusLineNC = hl "gray" null {bold = true;};
-    StorageClass = hl "purple" null {};
-    String = hl "green" null {};
-    Structure = hl "yellow" null {};
-    Substitute = hl "dark-gray" "orange" {};
-    TabLine = hl "light-gray" "dark-gray" {};
-    TabLineFill = hl "light-gray" "dark-gray" {};
-    TabLineSel = hl "blue" "dark-gray" {bold = true;};
-    Tag = hl "yellow" null {};
-    Title = hl "blue" null {};
-    Todo = hl "yellow" "dark-gray" {
-      bold = true;
-      italic = true;
-    };
-    TooLong = hl "red" null {};
-    Twilight = hl "gray" null {};
-    Type = hl "yellow" null {};
-    Typedef = hl "yellow" null {};
-    Underlined = hl "red" null {};
-    VertSplit = hl "gray" null {};
-    VirtualText = hl "gray" null {italic = true;};
-    Visual = hl null "dark-gray" {};
-    VisualNOS = hl "red" null {};
-    Warning = hl "yellow" null {bold = true;};
-    Whitespace = hl "gray" null {};
-    WildMenu = hl "red" null {};
 
-    # links
-    "@text.diff.add" = link "DiffAdd";
-    "@text.diff.delete" = link "DiffDelete";
-    "@text.emphasis" = link "Italic";
-    "@text.strong" = link "Bold";
-    ErrorMsg = link "Error";
-    FloatermBorder = link "FloatBorder";
-    GitSignsCurrentLineBlame = link "VirtualText";
-    InfoMsg = link "Info";
-    LspInfoBorder = link "FloatBorder";
-    MiniIndentscopeSymbol = link "Comment";
-    NormalFloat = link "Normal";
-    NvimInternalError = link "Error";
-    SpellLocal = link "SpellRare";
-    TelescopeBorder = link "FloatBorder";
-    WarningMsg = link "Warning";
-    diffAdded = link "DiffAdd";
-    diffRemoved = link "DiffDelete";
+  # transform an attr set of { ${src} = [ "dst1" "dst2" ... ]; ... } into
+  # a set of { dst1 = link src; dst2 = link src; ... }
+  links = set:
+    concatMapAttrs
+    (src: dsts:
+      listToAttrs
+      (map
+        (dst: nameValuePair dst (link src))
+        dsts))
+    set;
+
+  state = {
+    debug = "purple";
+    error = "red";
+    hint = "yellow";
+    info = "cyan";
+    ok = "green";
+    trace = "brown";
+    warning = "orange";
   };
+in {
+  programs.nixvim.highlight =
+    {
+      # bars
+      BarPill = hl "dark-gray" null {};
+      CustomBlueStatus = hl "blue" "dark-gray" {bold = true;};
+      CustomCyanStatus = hl "cyan" "dark-gray" {bold = true;};
+      CustomFuchsiaStatus = hl "purple" "dark-gray" {bold = true;};
+      CustomLimeStatus = hl "green" "dark-gray" {bold = true;};
+      CustomRedStatus = hl "red" "dark-gray" {bold = true;};
+      CustomYellowStatus = hl "yellow" "dark-gray" {bold = true;};
+      StatusLine = hl "light-gray" "dark-gray" {italic = true;};
+      StatusLineNC = hl "gray" null {bold = true;};
+      TabLine = hl "light-gray" "dark-gray" {};
+      TabLineFill = hl "light-gray" "dark-gray" {};
+      TabLineSel = hl "blue" "dark-gray" {bold = true;};
+
+      # cmp
+      CmpItemAbbr = hl "light-gray" null {};
+      CmpItemAbbrMatch = hl "cyan" null {bold = true;};
+      CmpItemAbbrMatchFuzzy = hl "blue" null {};
+      CmpItemKind = hl "purple" null {};
+      CmpItemMenu = hl "gray" null {};
+
+      # diagnostics
+      DiagnosticError = hl state.error null {};
+      DiagnosticHint = hl state.hint null {};
+      DiagnosticInfo = hl state.info null {};
+      DiagnosticOk = hl state.ok null {};
+      DiagnosticWarn = hl state.warning null {};
+      DiagnosticUnderlineError = hl null null {
+        undercurl = true;
+        sp = nameToHex state.error;
+      };
+      DiagnosticUnderlineHint = hl null null {
+        undercurl = true;
+        sp = nameToHex state.hint;
+      };
+      DiagnosticUnderlineInfo = hl null null {
+        undercurl = true;
+        sp = nameToHex state.info;
+      };
+      DiagnosticUnderlineOk = hl null null {
+        underline = true;
+        sp = nameToHex state.ok;
+      };
+      DiagnosticUnderlineWarn = hl null null {
+        undercurl = true;
+        sp = nameToHex state.warning;
+      };
+
+      # diffs
+      DiffAdd = hl "green" "dark-gray" {bold = true;};
+      DiffChange = hl null null {};
+      DiffDelete = hl "red" "dark-gray" {bold = true;};
+      DiffText = hl "yellow" "dark-gray" {bold = true;};
+      GitGutterAdd = hl "green" null {bold = true;};
+      GitGutterChange = hl "orange" null {bold = true;};
+      GitGutterDelete = hl "red" null {bold = true;};
+
+      # notifications
+      NotifyBackground = hl "dark-gray" null {};
+      NotifyDEBUGTitle = hl state.debug null {bold = true;};
+      NotifyERRORTitle = hl state.error null {bold = true;};
+      NotifyINFOTitle = hl state.info null {bold = true;};
+      NotifyTRACETitle = hl state.trace null {bold = true;};
+      NotifyWARNTitle = hl null null {bold = true;};
+
+      # state
+      Debug = hl state.debug null {};
+      Error = hl state.error null {};
+      Info = hl state.info null {};
+      Warning = hl state.warning null {};
+      Trace = hl state.trace null {};
+
+      # syntax
+      "@neorg.headings.1.prefix" = hl "brown" null {};
+      "@neorg.headings.1.title" = hl "brown" null {};
+      "@neorg.markup.verbatim" = hl "orange" null {};
+      "@text.literal" = hl "green" null {};
+      Bold = hl null null {bold = true;};
+      Boolean = hl "orange" null {};
+      Character = hl "red" null {};
+      Comment = hl "light-gray" null {};
+      Conceal = hl "gray" null {};
+      Conditional = hl "brown" null {};
+      Constant = hl "orange" null {};
+      Define = hl "purple" null {};
+      Delimiter = hl "light-silver" null {};
+      Directory = hl "blue" null {};
+      Float = hl "orange" null {};
+      Function = hl "blue" null {};
+      Identifier = hl "red" null {};
+      IncSearch = hl "black" "yellow" {};
+      Include = hl "blue" null {};
+      Italic = hl null null {italic = true;};
+      Keyword = hl "purple" null {};
+      Label = hl "yellow" null {};
+      Macro = hl "red" null {};
+      MatchParen = hl "bright-white" "dark-gray" {bold = true;};
+      Number = hl "orange" null {};
+      Operator = hl "light-silver" null {};
+      PreProc = hl "yellow" null {};
+      Question = hl "blue" null {};
+      Repeat = hl "yellow" null {};
+      Special = hl "cyan" null {};
+      SpecialChar = hl "brown" null {};
+      SpecialKey = hl "light-gray" null {};
+      SpellBad = hl "red" null {undercurl = true;};
+      SpellCap = hl "purple" null {};
+      SpellRare = hl "yellow" null {undercurl = true;};
+      Statement = hl "red" null {};
+      StorageClass = hl "purple" null {};
+      String = hl "green" null {};
+      Structure = hl "yellow" null {};
+      Substitute = hl "dark-gray" "orange" {};
+      Tag = hl "yellow" null {};
+      Title = hl "blue" null {};
+      Todo = hl "yellow" "dark-gray" {
+        bold = true;
+        italic = true;
+      };
+      TooLong = hl "red" null {};
+      Type = hl "yellow" null {};
+      Typedef = hl "yellow" null {};
+      Underlined = hl "red" null {};
+      Whitespace = hl "gray" null {};
+
+      # ui
+      ColorColumn = hl null "dark-gray" {};
+      Cursor = hl "black" "light-silver" {};
+      CursorColumn = hl null "dark-gray" {};
+      CursorLine = hl null "black" {};
+      CursorLineNr = hl "silver" null {bold = true;};
+      FloatBorder = hl "gray" null {};
+      FoldColumn = hl "cyan" null {};
+      Folded = hl "gray" "black" {};
+      LineNr = hl "light-gray" null {};
+      ModeMsg = hl "green" null {};
+      MoreMsg = hl "green" null {};
+      NonText = hl "light-gray" null {};
+      Normal = hl "light-silver" null {};
+      Pmenu = hl "light-silver" "dark-gray" {};
+      PmenuSbar = hl null null {};
+      PmenuSel = hl "dark-gray" "light-silver" {bold = true;};
+      PmenuThumb = hl "light-silver" "light-silver" {};
+      QuickFixLine = hl null "dark-gray" {};
+      Search = hl "dark-gray" "orange" {};
+      SignColumn = hl null null {};
+      Twilight = hl "gray" null {};
+      VertSplit = hl "gray" null {};
+      VirtualText = hl "gray" null {italic = true;};
+      Visual = hl null "dark-gray" {};
+      VisualNOS = hl "red" null {};
+      WildMenu = hl "red" null {};
+    }
+    // links {
+      Bold = ["@text.strong"];
+      Comment = ["MiniIndentscopeSymbol"];
+      Debug = ["NotifyDEBUGBorder" "NotifyDEBUGIcon"];
+      Error = ["ErrorMsg" "NotifyERRORBorder" "NotifyERRORIcon" "NvimInternalError"];
+      FloatBorder = ["FloatermBorder" "LspInfoBorder" "TelescopeBorder"];
+      Info = ["InfoMsg" "NotifyINFOBorder" "NotifyINFOIcon"];
+      Italic = ["@text.emphasis"];
+      Normal = ["NormalFloat"];
+      SpellRare = ["SpellLocal"];
+      Trace = ["NotifyTRACEBorder" "NotifyTRACEIcon"];
+      VirtualText = ["GitSignsCurrentLineBlame"];
+      Warning = ["WarningMsg" "NotifyWARNBorder" "NotifyWARNIcon"];
+      DiffAdd = ["@diff.plus" "diffAdded"];
+      DiffDelete = ["@diff.minus" "diffRemoved"];
+    };
 }
