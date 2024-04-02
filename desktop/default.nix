5c9fa7230b9ebced97e159bd4ce185ac75fbe4cd
municorn <municorn@musicaloft.com>
2024-04-02 14:01:27 -0600

    feat(desktop): add muni_bot settings

--- desktop/default.nix
+++ desktop/default.nix
@@ -114,6 +114,15 @@
     muni_bot = {
       enable = true;
       environmentFile = config.sops.secrets."muni_bot.env".path;
+      settings = {
+        twitch = {
+          raid_msg_all = "🐎🦄🐎🦄🐎 STAMPEEEEEDE! IT'S A MUNICORN RAID!!! 🦄🐎🦄🐎🦄";
+          raid_msg_subs = "munico1Giggle munico1Hype munico1Wiggle STAMPEEEEEDE! IT'S A MUNICORN RAID!!! munico1Giggle munico1Hype munico1Wiggle";
+        };
+        discord.ventriloquists = [
+          633840621626458115
+        ];
+      };
     };
 
     psd.enable = true;
