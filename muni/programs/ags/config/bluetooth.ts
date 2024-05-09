4fd207760207c5c5a2ee58bb677d5c2d405c8fa9
municorn <municorn@musicaloft.com>
2024-05-09 10:00:16 -0600

    feat(ags): add bluetooth battery indicator

--- muni/programs/ags/config/bluetooth.ts
+++ muni/programs/ags/config/bluetooth.ts
@@ -1,49 +1,71 @@
-import { Tile, makeTile } from "utils"
+import { Tile, makeTile } from "utils";
 
-const bluetooth = await Service.import('bluetooth')
+const bluetooth = await Service.import("bluetooth");
+
+const BLUETOOTH_BATTERY_ICONS = [
+  "\u{F093E}",
+  "\u{F093F}",
+  "\u{F0940}",
+  "\u{F0941}",
+  "\u{F0942}",
+  "\u{F0943}",
+  "\u{F0944}",
+  "\u{F0945}",
+  "\u{F0946}",
+  "\u{F0948}",
+];
+const BLUETOOTH_BATTERY_UNKNOWN_ICON = "\u{F094A}";
 
 export function Bluetooth() {
-  const tileBinding = Utils.merge([
-    bluetooth.bind("enabled"),
-    bluetooth.bind("connected_devices")
-  ], (enabled, connectedDevices): Tile => {
-    if (!enabled) {
-      return {
-        icon: "\u{F00B2}",
-        primary: "",
-        secondary: "",
-        visible: true,
+  const tileBinding = Utils.merge(
+    [bluetooth.bind("enabled"), bluetooth.bind("connected_devices")],
+    (enabled, connectedDevices): Tile => {
+      if (!enabled) {
+        return {
+          icon: "\u{F00B2}",
+          primary: "",
+          secondary: "",
+          visible: true,
+        };
+      } else {
+        switch (connectedDevices.length) {
+          case 0:
+            return {
+              icon: "\u{F00AF}",
+              primary: "",
+              secondary: "",
+              visible: true,
+            };
+          case 1:
+            const device = connectedDevices[0];
+            const battery = device.battery_percentage || device.battery_level;
+            return {
+              icon: "\u{F00B1}",
+              primary: connectedDevices[0].name,
+              secondary:
+                battery > 0
+                  ? BLUETOOTH_BATTERY_ICONS[
+                      Math.min(
+                        BLUETOOTH_BATTERY_ICONS.length - 1,
+                        (BLUETOOTH_BATTERY_ICONS.length * battery) / 100,
+                      )
+                    ] || BLUETOOTH_BATTERY_UNKNOWN_ICON
+                  : "",
+              visible: true,
+            };
+          default:
+            return {
+              icon: "\u{F00B1}",
+              primary: `${connectedDevices.length} devices`,
+              secondary: "",
+              visible: true,
+            };
+        }
       }
-    } else {
-      switch (connectedDevices.length) {
-        case 0:
-          return {
-            icon: "\u{F00AF}",
-            primary: "",
-            secondary: "",
-            visible: true,
-          }
-        case 1:
-          return {
-            icon: "\u{F00B1}",
-            primary: connectedDevices[0].name,
-            secondary: "",
-            visible: true,
-          }
-        default:
-          return {
-            icon: "\u{F00B1}",
-            primary: `${connectedDevices.length} devices`,
-            secondary: "",
-            visible: true,
-          }
-      }
-    }
+    },
+  );
 
-
-  });
-
-  return makeTile(tileBinding)
+  return makeTile(tileBinding);
 }
 
 export {};
