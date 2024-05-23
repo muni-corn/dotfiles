b8db46910a1c3f4b8a53540c397620396c5ce36a
municorn <municorn@musicaloft.com>
2024-05-22 22:00:58 -0600

    feat(desktop): add btrbk instance for remote backups

--- desktop/btrbk.nix
+++ desktop/btrbk.nix
@@ -27,5 +27,25 @@
         };
       };
     };
+
+    remotebackup = {
+      onCalendar = "hourly";
+      settings = {
+        snapshot_create = "no";
+        target_preserve = "2h 14d 8w 12m *y";
+        target_preserve_min = "30m";
+        preserve_hour_of_day = "5";
+
+        ssh_identity = "/etc/ssh/btrbk_ed25519";
+        ssh_user = "btrbk";
+        stream_compress = "zstd";
+
+        volume."/" = {
+          subvolume.home = {};
+          target = "ssh://192.168.68.70/crypt/backup/ponycastle";
+          snapshot_dir = "/snaps";
+        };
+      };
+    };
   };
 }
