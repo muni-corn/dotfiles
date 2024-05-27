bfa37c7218dbd78468e4ba9695afb8e8455b836a
municorn <municorn@musicaloft.com>
2024-05-26 22:17:10 -0600

    refactor(nvim): remove unused parameters in modules

--- muni/programs/nvim/default.nix
+++ muni/programs/nvim/default.nix
@@ -1,8 +1,4 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
   imports = [
     ./autocmd.nix
     ./globals.nix
--- muni/programs/nvim/plugins/default.nix
+++ muni/programs/nvim/plugins/default.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./cmp.nix
     ./copilot.nix
