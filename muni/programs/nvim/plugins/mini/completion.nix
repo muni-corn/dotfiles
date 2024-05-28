062fdfe3b84a2ac39ceb8e4d60ef57064978a0b8
municorn <municorn@musicaloft.com>
2024-05-28 15:05:47 -0600

    feat(nvim): make completion windows rounded

--- muni/programs/nvim/plugins/mini/completion.nix
+++ muni/programs/nvim/plugins/mini/completion.nix
@@ -1,3 +1,6 @@
 {
-  programs.nixvim.plugins.mini.modules.completion.__empty = null;
+  programs.nixvim.plugins.mini.modules.completion.window = {
+    info.border = "rounded";
+    signature.border = "rounded";
+  };
 }
