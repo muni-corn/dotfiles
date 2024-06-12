bf0a4754db2b202a5bd9537c51ee858f10f371f7
municorn <municorn@musicaloft.com>
2024-06-11 20:53:04 -0600

    feat(nas): add nextcloud container to nas

--- nas/default.nix
+++ nas/default.nix
@@ -12,6 +12,42 @@
     ../sops
   ];
 
+  containers = {
+    nextcloud = {
+      autoStart = true;
+      bindMounts = {
+        nextcloud = {
+          hostPath = "/crypt/nextcloud";
+          mountPoint = "/var/lib/nextcloud";
+          isReadOnly = false;
+        };
+      };
+      config = {...}: {
+        services = {
+          nextcloud = {
+            enable = true;
+            package = pkgs.nextcloud28;
+
+            caching.redis = true;
+            config = {
+              adminpassFile = ''${pkgs.writeText "adminpass" "weakpass"}'';
+              dbtype = "pgsql";
+            };
+            database.createLocally = true;
+            hostName = "cloud.musicaloft.com";
+            configureRedis = true;
+            maxUploadSize = "10G";
+          };
+          postgresql.package = pkgs.postgresql_16;
+        };
+      };
+    };
+    postfix = {
+      enable = true;
+      hostname = "mail.musicaloft.com";
+    };
+  };
+
   # Enable networking
   networking = {
     hostName = "spiritcrypt"; # Define your hostname.
