6b5c9c7745e116562a3806b9dc52f0d8cbca0273
municorn <municorn@musicaloft.com>
2024-08-12 13:59:16 -0600

    feat(server): disable nvidia open-source drivers for now

--- server/hardware.nix
+++ server/hardware.nix
@@ -93,6 +93,6 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware = {
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-    nvidia.open = true;
+    nvidia.open = lib.mkForce false;
   };
 }
