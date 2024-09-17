2d0497885b47beb8f4c263593c74c950d6906bfe
municorn <municorn@musicaloft.com>
2024-09-17 09:34:59 -0600

    feat(nvim): configure terminal buffer colors

--- muni/programs/nvim/globals.nix
+++ muni/programs/nvim/globals.nix
@@ -1,4 +1,4 @@
-{
+{config, ...}: {
   programs.nixvim.globals = {
     mapleader = ",";
 
@@ -16,5 +16,23 @@
     neovide_floating_z_height = 10;
     neovide_floating_blur_amount_x = 2.0;
     neovide_floating_blur_amount_y = 2.0;
+
+    # terminal buffer colors
+    terminal_color_0 = "#" + config.muse.theme.palette.black;
+    terminal_color_1 = "#" + config.muse.theme.palette.red;
+    terminal_color_2 = "#" + config.muse.theme.palette.green;
+    terminal_color_3 = "#" + config.muse.theme.palette.yellow;
+    terminal_color_4 = "#" + config.muse.theme.palette.blue;
+    terminal_color_5 = "#" + config.muse.theme.palette.purple;
+    terminal_color_6 = "#" + config.muse.theme.palette.cyan;
+    terminal_color_7 = "#" + config.muse.theme.palette.silver;
+    terminal_color_8 = "#" + config.muse.theme.palette.gray;
+    terminal_color_9 = "#" + config.muse.theme.palette.bright-red;
+    terminal_color_10 = "#" + config.muse.theme.palette.bright-green;
+    terminal_color_11 = "#" + config.muse.theme.palette.bright-yellow;
+    terminal_color_12 = "#" + config.muse.theme.palette.bright-blue;
+    terminal_color_13 = "#" + config.muse.theme.palette.bright-purple;
+    terminal_color_14 = "#" + config.muse.theme.palette.bright-cyan;
+    terminal_color_15 = "#" + config.muse.theme.palette.white;
   };
 }
