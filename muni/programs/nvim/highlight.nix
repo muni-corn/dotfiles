9633f41699468230a03dba45f647dbcf7dbce252
municorn <municorn@musicaloft.com>
2024-10-26 10:06:52 -0600

    feat(nvim): reintroduce highlights under `highlightOverride`

--- muni/programs/nvim/highlight.nix
+++ muni/programs/nvim/highlight.nix
@@ -1,8 +1,12 @@
 {
   config,
+  lib,
   inputs,
   ...
 }: let
+  inherit (builtins) listToAttrs map;
+  inherit (lib.attrsets) concatMapAttrs nameValuePair;
+
   nc = inputs.nix-colorizer;
 
   lighten = hex: nc.oklchToHex (nc.lighten (nc.hexToOklch hex) 25);
@@ -61,6 +65,97 @@
         else "NONE";
     }
     // attrs;
+  link = to: {link = to;};
+
+  # transform an attr set of { ${src} = [ "dst1" "dst2" ... ]; ... } into
+  # a set of { dst1 = link src; dst2 = link src; ... }
+  links = set:
+    concatMapAttrs
+    (src: dsts:
+      listToAttrs
+      (map
+        (dst: nameValuePair dst (link src))
+        dsts))
+    set;
+
+  stateColors = {
+    debug = "magenta";
+    error = "red";
+    hint = "cyan";
+    info = "blue";
+    ok = "green";
+    trace = "brown";
+    warning = "yellow";
+  };
 in {
-  programs.nixvim.highlight.Twilight = hl "gray" null {};
+  programs.nixvim.highlightOverride =
+    {
+      # diagnostics
+      DiagnosticError = hl stateColors.error null {};
+      DiagnosticHint = hl stateColors.hint null {};
+      DiagnosticInfo = hl stateColors.info null {};
+      DiagnosticOk = hl stateColors.ok null {};
+      DiagnosticWarn = hl stateColors.warning null {};
+      DiagnosticFloatingError = hl stateColors.error "dark-gray" {};
+      DiagnosticFloatingHint = hl stateColors.hint "dark-gray" {};
+      DiagnosticFloatingInfo = hl stateColors.info "dark-gray" {};
+      DiagnosticFloatingOk = hl stateColors.ok "dark-gray" {};
+      DiagnosticFloatingWarn = hl stateColors.warning "dark-gray" {};
+      DiagnosticUnderlineError = hl null null {
+        undercurl = true;
+        sp = hexOf stateColors.error;
+      };
+      DiagnosticUnderlineHint = hl null null {
+        underdashed = true;
+        sp = hexOf stateColors.hint;
+      };
+      DiagnosticUnderlineInfo = hl null null {
+        underdashed = true;
+        sp = hexOf stateColors.info;
+      };
+      DiagnosticUnderlineOk = hl null null {
+        underdotted = true;
+        sp = hexOf stateColors.ok;
+      };
+      DiagnosticUnderlineWarn = hl null null {
+        undercurl = true;
+        sp = hexOf stateColors.warning;
+      };
+
+      # diffs
+      Added = hl "green" null {};
+      Changed = hl "orange" null {};
+      DiffAdd = hl "bright-green" "dark-green" {bold = true;};
+      DiffChange = hl null null {};
+      DiffDelete = hl "bright-red" "dark-red" {bold = true;};
+      DiffText = hl "bright-yellow" "dark-yellow" {bold = true;};
+      Removed = hl "red" null {};
+
+      # state
+      Debug = hl stateColors.debug null {};
+      Error = hl stateColors.error null {};
+      Info = hl stateColors.info null {};
+      Warning = hl stateColors.warning null {};
+      Trace = hl stateColors.trace null {};
+
+      # ui
+      CursorLineNr = hl "light-silver" "black" {bold = true;};
+      IncSearch = hl "bright-yellow" "dark-yellow" {};
+      MiniCursorword = hl null "dark-gray" {};
+      Search = hl "bright-orange" "dark-orange" {};
+      Substitute = hl "bright-red" "dark-red" {};
+      Todo = hl "yellow" "dark-gray" {
+        bold = true;
+        italic = true;
+      };
+      Twilight = hl "gray" null {};
+    }
+    // links {
+      Comment = ["MiniIndentscopeSymbol"];
+      Error = ["TooLong"];
+      Todo = ["@comment.todo"];
+      DiffAdd = ["@diff.plus" "diffAdded"];
+      DiffDelete = ["@diff.minus" "diffRemoved"];
+      IncSearch = ["CurSearch"];
+    };
 }
