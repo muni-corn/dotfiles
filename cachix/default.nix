e1c523111329a6883f09313e0b22ea9cc6d72218
municorn <municorn@musicaloft.com>
2024-11-20 13:10:37 -0700

    feat: add binary cache for nixified ai

--- cachix/default.nix
+++ cachix/default.nix
@@ -4,4 +4,10 @@
     ./muni.nix
     ./nix-community.nix
   ];
+
+  nix.settings = {
+    trusted-substituters = [ "https://ai.cachix.org" ];
+    trusted-public-keys = [ "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc=" ];
+  };
+
 }
