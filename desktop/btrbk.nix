a92a41a3b23b54f4d9d94aea4d1110014364c512
municorn <municorn@musicaloft.com>
2024-11-24 13:44:32 -0700

    feat(desktop): preserve only 96h of snapshots

--- desktop/btrbk.nix
+++ desktop/btrbk.nix
@@ -5,7 +5,7 @@
       settings = {
         snapshot_create = "onchange";
         snapshot_preserve_min = "48h";
-        snapshot_preserve = "48h 28d 8w";
+        snapshot_preserve = "96h";
         preserve_hour_of_day = "5";
         volume."/" = {
           subvolume.home = {};
