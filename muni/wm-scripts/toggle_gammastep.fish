fc475bcdf36d4aa619f2e75c412ea5186f9f9f3a
municorn <municorn@musicaloft.com>
2024-12-11 10:18:11 -0700

    fix: replace `dunstify` calls with `notify-send`

--- muni/wm-scripts/toggle_gammastep.fish
+++ muni/wm-scripts/toggle_gammastep.fish
@@ -4,7 +4,7 @@ set -l lock_file_path $HOME/.toggle_gammastep.lock
 
 # create a lock file if it doesn't exist already.
 # if the lock file exists, it's likely that the script is already running
-if ! echo >? $lock_file_path ^/dev/null
+if ! echo >?$lock_file_path ^/dev/null
     echo "this script is busy"
     exit 1
 end
@@ -15,10 +15,10 @@ trap "rm -f $lock_file_path" EXIT
 set -l feature_name "Night Display"
 
 if systemctl --user --quiet is-active gammastep
-    set nid (dunstify -p -u low "Turning $feature_name off" "Turn $feature_name on before bed to help you sleep better.")
+    set nid (notify-send -p -u low "Turning $feature_name off" "Turn $feature_name on before bed to help you sleep better.")
     systemctl --user stop gammastep
-    and dunstify -u low -t 5000 -r $nid "$feature_name is now off" "Turn $feature_name on before bed to help you sleep better."
+    and notify-send -u low -t 5000 -r $nid "$feature_name is now off" "Turn $feature_name on before bed to help you sleep better."
 else
     systemctl --user start gammastep
-    and dunstify -u low -t 5000 "$feature_name is now on" "$feature_name filters blue light to help you sleep when it's late."
+    and notify-send -u low -t 5000 "$feature_name is now on" "$feature_name filters blue light to help you sleep when it's late."
 end
