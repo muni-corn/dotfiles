58246ac719ba4cba86c5a880db7a6f90a3eec987
municorn <municorn@musicaloft.com>
2024-12-11 10:16:28 -0700

    refactor: delete capture scripts

--- muni/wm-scripts/capture/post-capture.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-# start services that were stopped before
-systemctl --user start syncthing.service
-systemctl --user start hydroxide.service
-systemctl --user start spotifyd.service
-systemctl --user start swayidle.service
-systemctl --user start kbfs.service
-systemctl --user start keybase.service
--- muni/wm-scripts/capture/pre-capture.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-# stop services
-systemctl --user stop syncthing.service
-systemctl --user stop hydroxide.service
-systemctl --user stop spotifyd.service
-systemctl --user stop swayidle.service
-systemctl --user stop kbfs.service
-systemctl --user stop keybase.service
