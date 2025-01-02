73899e27a7db7b0bf94686d95a3045785f21a01a
municorn <municorn@musicaloft.com>
2025-01-02 09:54:53 -0700

    feat(desktop): add config for vr

---
+++ vr.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # rule for Quest 3
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="2833", ATTRS{idProduct}=="0186", RUN+="${config.systemd.package}/bin/systemctl start vr-adb.service"
+  '';
+
+  systemd.services.vr-adb = {
+    description = "adb service for VR headsets";
+    path = with pkgs; [
+      android-tools
+      bash
+    ];
+    script = ''
+      echo "sleeping first"
+      sleep 5
+      echo "executing 'adb devices'"
+      adb devices
+      echo "executing 'adb forward' for ports 9943 and 9944"
+      adb forward tcp:9943 tcp:9943
+      adb forward tcp:9944 tcp:9944
+    '';
+    serviceConfig.Type = "oneshot";
+  };
+}
