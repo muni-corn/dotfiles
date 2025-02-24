fa7da2e3e8e30d6d6296999abd084e139cf8d2ea
municorn <municorn@musicaloft.com>
2025-02-24 10:50:15 -0700

    refactor(desktop): combine common attrset parents

--- desktop/default.nix
+++ desktop/default.nix
@@ -22,20 +22,22 @@
     ./vfio.nix
   ];
 
-  boot.loader = {
-    efi.efiSysMountPoint = "/boot/efi";
-    systemd-boot.memtest86.enable = true;
+  boot = {
+    loader = {
+      efi.efiSysMountPoint = "/boot/efi";
+      systemd-boot.memtest86.enable = true;
+    };
+    kernelPatches = [
+      {
+        name = "amdgpu-ignore-ctx-privileges";
+        patch = pkgs.fetchpatch {
+          name = "cap_sys_nice_begone.patch";
+          url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+          hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+        };
+      }
+    ];
   };
-  boot.kernelPatches = [
-    {
-      name = "amdgpu-ignore-ctx-privileges";
-      patch = pkgs.fetchpatch {
-        name = "cap_sys_nice_begone.patch";
-        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-      };
-    }
-  ];
 
   hardware = {
     amdgpu = {
