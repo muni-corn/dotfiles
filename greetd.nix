aa2d7a1b816963730e4568e062dc3cc18b836899
municorn <municorn@musicaloft.com>
2025-03-06 23:58:51 -0700

    feat(greetd): remember last session

--- greetd.nix
+++ greetd.nix
@@ -4,7 +4,8 @@
       enable = true;
       restart = false;
       settings.default_session = {
-        command = ''${pkgs.greetd.tuigreet}/bin/tuigreet -g "Welcome back" -r -t --time-format '%-I:%M %P  %a, %b %-d' --asterisks --power-shutdown 'systemctl poweroff' --power-reboot 'systemctl reboot' --cmd Hyprland'';
+        command = ''${pkgs.greetd.tuigreet}/bin/tuigreet -g "Welcome back <3" -r --remember-session --remember-user-session -t --time-format '%-I:%M %P  %a, %b %-d' --asterisks --power-shutdown 'systemctl poweroff' --power-reboot 'systemctl reboot' --cmd Hyprland'';
+        user = "greeter";
       };
     };
   };
