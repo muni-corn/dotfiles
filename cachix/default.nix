b42b049d26b4e4aa348bf03be53d913c823d231c
municorn <municorn@musicaloft.com>
2025-03-08 00:14:26 -0700

    style: format

--- cachix/default.nix
+++ cachix/default.nix
@@ -6,8 +6,7 @@
   ];
 
   nix.settings = {
-    trusted-substituters = [ "https://ai.cachix.org" ];
-    trusted-public-keys = [ "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc=" ];
+    trusted-substituters = ["https://ai.cachix.org"];
+    trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
   };
-
 }
