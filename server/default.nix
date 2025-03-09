56d080ac5ebc928333f16e7f10ba929d7f72cde0
municorn <municorn@musicaloft.com>
2025-03-08 19:22:49 -0700

    feat(server): upgrade nextcloud to 30

--- server/default.nix
+++ server/default.nix
@@ -29,7 +29,7 @@
         services = {
           nextcloud = {
             enable = true;
-            package = pkgs.nextcloud29;
+            package = pkgs.nextcloud30;
 
             caching.redis = true;
             config = {
