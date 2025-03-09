7c7f79485d39a855f52266e56542bba3778ee1c5
municorn <municorn@musicaloft.com>
2025-03-08 21:03:51 -0700

    fix(server): open firewall for hass

--- server/home-assistant.nix
+++ server/home-assistant.nix
@@ -1,6 +1,7 @@
-{...}: {
+{
   services.home-assistant = {
     enable = true;
+    openFirewall = true;
     config.homeassistant = {
       unit_system = "us_customary";
       time_zone = "America/Boise";
