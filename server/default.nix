058437464abae14f7144d5ec3b5093982e106bb0
municorn <municorn@musicaloft.com>
2025-03-12 17:06:20 -0600

    refactor: make i18n common

--- server/default.nix
+++ server/default.nix
@@ -19,23 +19,6 @@
   # Define your hostname.
   networking.hostName = "spiritcrypt";
 
-  i18n = {
-    # Select internationalisation properties.
-    defaultLocale = "en_US.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
-
   # Set your time zone.
   time.timeZone = lib.mkForce "America/Boise";
 
