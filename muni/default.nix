acf7e5da638910373c4cca44e1528eb6de4f6064
municorn <municorn@musicaloft.com>
2025-03-18 12:46:13 -0600

    refactor: move xdg config to non-graphical

--- muni/default.nix
+++ muni/default.nix
@@ -83,4 +83,44 @@
     enable = true;
     package = pkgs.taskwarrior3;
   };
+
+  xdg =
+    let
+      toml = pkgs.formats.toml { };
+    in
+    {
+      enable = true;
+      configFile = {
+        "peaclock.conf".source = ./peaclock.conf;
+        "rustfmt/rustfmt.toml".source = toml.generate "rustfmt-config" {
+          condense_wildcard_suffixes = true;
+          edition = "2021";
+          format_code_in_doc_comments = true;
+          format_macro_bodies = true;
+          format_macro_matchers = true;
+          group_imports = "StdExternalCrate";
+          imports_granularity = "Crate";
+          normalize_comments = true;
+          normalize_doc_attributes = true;
+          reorder_impl_items = true;
+          use_field_init_shorthand = true;
+          use_try_shorthand = true;
+          wrap_comments = true;
+        };
+        "gitui/key_bindings.ron".text = ''
+          (
+              move_left: Some(( code: Char('h'), modifiers: "")),
+              move_right: Some(( code: Char('l'), modifiers: "")),
+              move_up: Some(( code: Char('k'), modifiers: "")),
+              move_down: Some(( code: Char('j'), modifiers: "")),
+
+              stash_open: Some(( code: Char('l'), modifiers: "")),
+              open_help: Some(( code: F(1), modifiers: "")),
+
+              status_reset_item: Some(( code: Char('U'), modifiers: "SHIFT")),
+          )
+        '';
+      };
+    };
+
 }
--- muni/graphical/default.nix
+++ muni/graphical/default.nix
@@ -127,27 +127,5 @@ in
     enable = true;
     platformTheme.name = "qtct";
   };
-
-  xdg = {
-    enable = true;
-    configFile = {
-      "peaclock.conf".source = ./peaclock.conf;
-      "rustfmt/rustfmt.toml".source = toml.generate "rustfmt-config" {
-        condense_wildcard_suffixes = true;
-        edition = "2021";
-        format_code_in_doc_comments = true;
-        format_macro_bodies = true;
-        format_macro_matchers = true;
-        group_imports = "StdExternalCrate";
-        imports_granularity = "Crate";
-        normalize_comments = true;
-        normalize_doc_attributes = true;
-        reorder_impl_items = true;
-        use_field_init_shorthand = true;
-        use_try_shorthand = true;
-        wrap_comments = true;
-      };
-    };
-  };
 }
 # vim: ts=2 sw=2 expandtab
