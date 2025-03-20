0697561715c84203e5497af4a9df566f49f14acd
municorn <municorn@musicaloft.com>
2025-03-20 01:00:53 -0600

    feat(zellij): start with default mode

--- muni/programs/zellij.nix
+++ muni/programs/zellij.nix
@@ -1,6 +1,7 @@
 {
 
   programs = {
+    # start with zellij if accessed by ssh
     fish.interactiveShellInit = ''
       if set -q SSH_TTY
         eval (zellij setup --generate-auto-start fish | string collect)
