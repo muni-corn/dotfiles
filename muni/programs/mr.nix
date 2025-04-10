7b946f06ddcba4b1bf620eac869bd2b65acf31a8
municorn <municorn@musicaloft.com>
2025-04-10 10:20:09 -0600

    feat(mr): add nix-templates repo

--- muni/programs/mr.nix
+++ muni/programs/mr.nix
@@ -45,6 +45,7 @@
       "code/muse-shell".checkout = "git clone https://github.com/muni-corn/muse-shell";
       "code/muse-sounds".checkout = "git clone git@codeberg.org:municorn/muse-sounds";
       "code/musicaloft-web".checkout = "git clone git@github.com:musicaloft/musicaloft-web.git";
+      "code/nix-templates".checkout = "git clone git@github.com:muni-corn/nix-templates.git";
       "code/nixpkgs" = {
         checkout = "git clone git@github.com:muni-corn/nixpkgs.git";
         post_checkout = "git remote add upstream git@github.com:NixOS/nixpkgs";
