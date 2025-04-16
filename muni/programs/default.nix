d63bb08a172f77c54ed0be81646e87c967aed41b
municorn <municorn@musicaloft.com>
2025-04-15 22:37:01 -0600

    feat(programs): configure taskwarrior-tui

--- muni/programs/default.nix
+++ muni/programs/default.nix
@@ -78,6 +78,15 @@
         search.case.sensitive = "no";
         sync.server.url = "http://192.168.68.70:10222";
 
+        # set default filter for taskwarrior-tui
+        uda.taskwarrior-tui = {
+          selection = {
+            reverse = true;
+            italic = true;
+          };
+          task-report.next.filter = "status:pending";
+        };
+
         # remove news popup
         verbose = "affected,blank,context,edit,header,footnote,label,new-id,project,special,sync,override,recur";
       };
