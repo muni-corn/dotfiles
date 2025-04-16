28fd1e42fc0d50c9fb0b2ed327ba3b93b17893e6
municorn <municorn@musicaloft.com>
2025-04-16 10:48:04 -0600

    chore: remove useless email module

--- muni/graphical/email/mod.nix
+++
@@ -1,8 +0,0 @@
-{ config, ... }:
-{
-  maildirBasePath = "${config.home.homeDirectory}/.mail";
-  accounts = {
-    work = import ../secret/gmail_work.nix;
-    protonmail = import ../secret/protonmail.nix;
-  };
-}
