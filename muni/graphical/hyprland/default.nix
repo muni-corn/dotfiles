074ad042fb9ccb2c248594f8d56a583a61e5dd6e
municorn <municorn@musicaloft.com>
2025-05-12 00:46:36 -0600

    revert(hyprland): adjust anr pings and enforce permissions
    This reverts commit 0c3588f7bf5fa814ad85692a45fc1b0d98fee937.

--- muni/graphical/hyprland/default.nix
+++ muni/graphical/hyprland/default.nix
@@ -80,8 +80,6 @@
           };
         };
 
-        ecosystem.enforce_permissions = true;
-
         input = {
           numlock_by_default = true;
           natural_scroll = false;
@@ -108,7 +106,6 @@
         # misc
         misc = {
           allow_session_lock_restore = true;
-          anr_missed_pings = 3;
           disable_splash_rendering = true;
           enable_swallow = true;
           focus_on_activate = true;
