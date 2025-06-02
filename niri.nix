c431883871a24a376e348c4abf2cfa89152edff3
municorn <municorn@musicaloft.com>
2025-06-02 09:19:05 -0600

    feat(niri): disable some things set by the flake

--- niri.nix
+++ niri.nix
@@ -1,8 +1,10 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [ xwayland-satellite-unstable ];
   programs.niri = {
     enable = true;
     package = pkgs.niri-unstable;
   };
+  services.gnome.gnome-keyring.enable = lib.mkForce false;
+  systemd.user.services.niri-flake-polkit.enable = false;
 }
