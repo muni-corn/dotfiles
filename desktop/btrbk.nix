51ee34766f356653522a3b5869527bf5188f0374
municorn <municorn@musicaloft.com>
2025-06-04 14:56:45 -0600

    feat(btrbk): update config
    - add `mbuffer` package
    - add `archive_preserve_min` option
    - take remote backups every hour

--- desktop/btrbk.nix
+++ desktop/btrbk.nix
@@ -1,50 +1,55 @@
+{ pkgs, ... }:
 {
-  services.btrbk.instances = {
-    snapshots = {
-      onCalendar = "*:00/5";
-      settings = {
-        snapshot_create = "onchange";
-        snapshot_preserve_min = "48h";
-        snapshot_preserve = "96h 7d 2w";
-        preserve_hour_of_day = "5";
-        volume."/home" = {
-          subvolume = {
-            muni = { };
-            "muni/.local" = { };
-            "muni/Documents" = { };
-            "muni/Music" = { };
-            "muni/Pictures" = { };
-            "muni/Videos" = { };
-            "muni/code" = { };
+  services.btrbk = {
+    extraPackages = with pkgs; [ mbuffer ];
+    instances = {
+      snapshots = {
+        onCalendar = "*:00/5";
+        settings = {
+          snapshot_create = "onchange";
+          snapshot_preserve_min = "48h";
+          snapshot_preserve = "96h 7d 2w";
+          preserve_hour_of_day = "5";
+          volume."/home" = {
+            subvolume = {
+              muni = { };
+              "muni/.local" = { };
+              "muni/Documents" = { };
+              "muni/Music" = { };
+              "muni/Pictures" = { };
+              "muni/Videos" = { };
+              "muni/code" = { };
+            };
+            snapshot_dir = "/snaps";
           };
-          snapshot_dir = "/snaps";
         };
       };
-    };
-    remotebackup = {
-      onCalendar = "18:00";
-      settings = {
-        snapshot_create = "no";
-        target_preserve = "2h 14d 8w 12m *y";
-        target_preserve_min = "5m";
-        preserve_hour_of_day = "5";
+      remotebackup = {
+        onCalendar = "*:00";
+        settings = {
+          archive_preserve_min = "12m *y";
+          snapshot_create = "no";
+          target_preserve = "2h 14d 8w 12m *y";
+          target_preserve_min = "5m";
+          preserve_hour_of_day = "5";
 
-        ssh_identity = "/etc/ssh/btrbk_ed25519";
-        ssh_user = "btrbk";
-        stream_compress = "zstd";
+          ssh_identity = "/etc/ssh/btrbk_ed25519";
+          ssh_user = "btrbk";
+          stream_compress = "zstd";
 
-        volume."/home" = {
-          subvolume = {
-            muni = { };
-            "muni/.local" = { };
-            "muni/Documents" = { };
-            "muni/Music" = { };
-            "muni/Pictures" = { };
-            "muni/Videos" = { };
-            "muni/code" = { };
+          volume."/home" = {
+            subvolume = {
+              muni = { };
+              "muni/.local" = { };
+              "muni/Documents" = { };
+              "muni/Music" = { };
+              "muni/Pictures" = { };
+              "muni/Videos" = { };
+              "muni/code" = { };
+            };
+            target = "ssh://192.168.68.70/crypt/backup/breezi/muni";
+            snapshot_dir = "/snaps";
           };
-          target = "ssh://192.168.68.70/crypt/backup/breezi/muni";
-          snapshot_dir = "/snaps";
         };
       };
     };
