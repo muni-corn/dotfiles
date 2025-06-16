954bb2317268ff70248fea210e39218470700dbe
municorn <municorn@musicaloft.com>
2025-06-16 00:42:56 -0600

    fix(attic): add port to firewall

--- server/attic.nix
+++ server/attic.nix
@@ -1,27 +1,27 @@
 { config, ... }:
+let
+  port = 8111;
+in
 {
-  services =
-    let
-      port = 8111;
-    in
-    {
-      atticd = {
-        enable = true;
-        environmentFile = config.sops.secrets.atticd_env.path;
-        settings.listen = "[::]:${builtins.toString port}";
-      };
+  networking.firewall.allowedTCPPorts = [ port ];
+  services = {
+    atticd = {
+      enable = true;
+      environmentFile = config.sops.secrets.atticd_env.path;
+      settings.listen = "[::]:${builtins.toString port}";
+    };
 
-      caddy = {
-        enable = true;
-        email = "caddy@musicaloft.com";
+    caddy = {
+      enable = true;
+      email = "caddy@musicaloft.com";
 
-        virtualHosts = {
-          "attic.musicaloft.com" = {
-            extraConfig = ''
-              reverse_proxy 127.0.0.1:${builtins.toString port}
-            '';
-          };
+      virtualHosts = {
+        "attic.musicaloft.com" = {
+          extraConfig = ''
+            reverse_proxy 127.0.0.1:${builtins.toString port}
+          '';
         };
       };
     };
+  };
 }
