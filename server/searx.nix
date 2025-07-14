879c9493d0147a733f68a489f9bd0d2bf3029d89
municorn <municorn@musicaloft.com>
2025-07-14 08:32:38 -0600

    feat(searx): add caddy reverse proxy configuration
    Add Caddy service configuration to proxy requests from search.musicaloft.com
    to the local Searx instance, enabling external access to the search service

--- server/searx.nix
+++ server/searx.nix
@@ -1,13 +1,26 @@
 { config, ... }:
 {
-  services.searx = {
-    enable = true;
-    environmentFile = config.sops.secrets.searx_env.path;
-    redisCreateLocally = true;
-    settings.server = {
-      port = 7327;
-      bind_address = "0.0.0.0";
-      secret_key = "@SEARX_SECRET_KEY@";
+  services = {
+    searx = {
+      enable = true;
+      environmentFile = config.sops.secrets.searx_env.path;
+      redisCreateLocally = true;
+      settings.server = {
+        port = 7327;
+        bind_address = "0.0.0.0";
+        secret_key = "@SEARX_SECRET_KEY@";
+      };
+    };
+
+    caddy = {
+      enable = true;
+      email = "caddy@musicaloft.com";
+
+      virtualHosts."search.musicaloft.com" = {
+        extraConfig = ''
+          reverse_proxy 127.0.0.1:${builtins.toString config.services.searx.settings.server.port}
+        '';
+      };
     };
   };
 }
