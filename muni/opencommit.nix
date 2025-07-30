e0e4663bc663093e2c3059d0ce78fee64af858e7
municorn <municorn@musicaloft.com>
2025-07-30 00:27:32 -0600

    feat(opencommit): add settings for better commit generation

--- muni/opencommit.nix
+++ muni/opencommit.nix
@@ -8,11 +8,14 @@
   home = {
     activation.setupOpencommit = lib.hm.dag.entryAfter [ "sops-nix" "writeBoundary" ] ''
       run oco config set OCO_AI_PROVIDER=anthropic
-      run oco config set OCO_TOKENS_MAX_INPUT=32768
-      run oco config set OCO_TOKENS_MAX_OUTPUT=1000
-      run oco config set OCO_EMOJI=false
-      run oco config set OCO_MODEL=claude-sonnet-4-0
       run oco config set OCO_API_KEY=$(cat ${config.sops.secrets.oco_api_key.path})
+      run oco config set OCO_MODEL=claude-sonnet-4-0
+
+      run oco config set OCO_DESCRIPTION=false
+      run oco config set OCO_EMOJI=false
+      run oco config set OCO_GITPUSH=false
+      run oco config set OCO_ONE_LINE_COMMIT=true
+      run oco config set OCO_TOKENS_MAX_INPUT=32768
     '';
     packages = with pkgs; [
       opencommit
