d2e2d2a365423f640cbe464f8249aa0a5af2dc30
municorn <municorn@musicaloft.com>
2025-07-29 18:00:16 -0600

    feat(opencode): add more mcp servers

--- muni/ai/opencode.nix
+++ muni/ai/opencode.nix
@@ -12,12 +12,61 @@
       small_model = "ollama/gemma3n";
 
       mcp = {
+        fetch = {
+          enabled = true;
+          type = "local";
+          command = [
+            "uvx"
+            "mcp-server-fetch"
+          ];
+        };
+
         context7 = {
           enabled = true;
           type = "remote";
           url = "https://mcp.context7.com/mcp";
         };
 
+        code-context = {
+          enabled = true;
+          type = "local";
+          command = [
+            "bun"
+            "x"
+            "code-context-provider-mcp@latest"
+          ];
+        };
+
+        memory = {
+          enabled = true;
+          type = "local";
+          command = [
+            "bun"
+            "x"
+            "@modelcontextprotocol/server-memory"
+          ];
+        };
+
+        nixos = {
+          enabled = true;
+          type = "local";
+          command = [
+            "nix"
+            "run"
+            "github:utensils/mcp-nixos"
+            "--"
+          ];
+        };
+
+        sequential-thinking = {
+          enabled = true;
+          type = "local";
+          command = [
+            "bun"
+            "x"
+            "@modelcontextprotocol/server-sequential-thinking"
+          ];
+        };
       };
 
       provider.ollama = {
