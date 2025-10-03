a24210e69cb992304c02dfeb255bcecc29b4d667
municorn <municorn@musicaloft.com>
2025-10-03 07:36:07 -0600

    feat(server): add beesd filesystem deduplication for crypt and main volumes

--- server/default.nix
+++ server/default.nix
@@ -62,6 +62,17 @@
   '';
 
   services = {
+    beesd.filesystems = {
+      crypt = {
+        spec = "UUID=90072e03-ea6e-414d-86cd-a36aa6a114fe";
+        hashTableSizeMB = 128;
+      };
+      main = {
+        spec = "UUID=0601e2c9-f445-4a4f-b656-281ae21c9b8c";
+        hashTableSizeMB = 128;
+      };
+    };
+
     btrfs.autoScrub = {
       enable = true;
       fileSystems = [ "/crypt" ];
