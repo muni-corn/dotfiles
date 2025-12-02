72768d9234daf8c9132d46344a7e7d9c009cdfc7
municorn <municorn@musicaloft.com>
2025-12-02 00:36:48 -0700

    feat(btrbk): update target preservation policies

--- desktop/btrbk.nix
+++ desktop/btrbk.nix
@@ -28,8 +28,8 @@
         settings = {
           archive_preserve = "12m *y";
           snapshot_create = "no";
-          target_preserve = "2h 14d 2w 12m *y";
-          target_preserve_min = "4w";
+          target_preserve = "2h 14d 4w 12m *y";
+          target_preserve_min = "latest";
           preserve_hour_of_day = "5";
 
           ssh_identity = "/etc/ssh/btrbk_ed25519";
