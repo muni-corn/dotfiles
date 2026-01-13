7fd3278530d223faee5a519a22fd50a1e6021f69
municorn <municorn@musicaloft.com>
2026-01-13 14:23:45 -0700

    fix(gaming): remove plasticscm since the build is failing

--- gaming.nix
+++ gaming.nix
@@ -1,61 +1,28 @@
 { pkgs, ... }:
 {
-  home-manager.users.muni.home.packages =
-    with pkgs;
-    let
-      plasticscm-client-core-unwrapped = pkgs.plasticscm-client-core-unwrapped.overrideAttrs (oldAttrs: {
-        src = fetchurl {
-          url = "https://www.plasticscm.com/plasticrepo/stable/debian/amd64/plasticscm-client-core_${oldAttrs.version}_amd64.deb";
-          hash = "sha256-/tfZLJ3a/6Jdk3opRKs+3/l09bFViN7/YuQ0hxVy4J8=";
-        };
-      });
-      plasticscm = (
-        plasticscm-client-complete.override {
-          plasticscm-client-core = pkgs.plasticscm-client-core.override {
-            inherit plasticscm-client-core-unwrapped;
-          };
-          plasticscm-client-gui = pkgs.plasticscm-client-gui.override {
-            plasticscm-client-gui-unwrapped = pkgs.plasticscm-client-gui-unwrapped.overrideAttrs (oldAttrs: {
-              src = fetchurl {
-                url = "https://www.plasticscm.com/plasticrepo/stable/debian/amd64/plasticscm-client-gui_${oldAttrs.version}_amd64.deb";
-                hash = "sha256-cilxGuy5Y6t/UImje0625qrfwgNp1gp7qKA1fpPcw2g=";
-              };
-            });
-            inherit plasticscm-client-core-unwrapped;
-            plasticscm-theme = pkgs.plasticscm-theme.overrideAttrs (oldAttrs: {
-              src = fetchurl {
-                url = "https://www.plasticscm.com/plasticrepo/stable/debian/amd64/plasticscm-theme_${oldAttrs.version}_amd64.deb";
-                hash = "sha256-gs9XGqpgxWue+Cke8x5FeyUDfQK8R/IrwWP59NRmubI=";
-              };
-            });
-          };
-        }
-      );
-    in
-    [
-      # classics
-      aisleriot
-      kdePackages.kmines
-      kdePackages.kpat
-      chess-tui
-      stockfish
+  home-manager.users.muni.home.packages = with pkgs; [
+    # classics
+    aisleriot
+    kdePackages.kmines
+    kdePackages.kpat
+    chess-tui
+    stockfish
 
-      # mods and compat tools
-      protonup-ng
-      protonup-qt
-      r2modman
+    # mods and compat tools
+    protonup-ng
+    protonup-qt
+    r2modman
 
-      # launchers
-      itch
-      prismlauncher
+    # launchers
+    itch
+    prismlauncher
 
-      # to configure mangohud
-      mangojuice
+    # to configure mangohud
+    mangojuice
 
-      # more for game development than gaming
-      unityhub
-      plasticscm
-    ];
+    # more for game development than gaming
+    unityhub
+  ];
 
   programs = {
     appimage = {
