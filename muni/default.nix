6572c30bd912efe6b1e0f78e6eba14fd1686016e
municorn <municorn@musicaloft.com>
2026-01-18 23:53:54 -0700

    feat: enable tmpfiles management for automatic directory creation and cleanup

--- muni/default.nix
+++ muni/default.nix
@@ -40,6 +40,25 @@ in
 
   services.tldr-update.enable = true;
 
+  systemd.user.tmpfiles.rules = [
+    # create the downloads subvolume with cleanup after 30 days
+    "v ${config.xdg.userDirs.download} - - - 30d -"
+
+    # automatically delete trash entires older than 30 days
+    "v ${config.xdg.dataHome}/Trash - - - 30d -"
+
+    # create xdg directories as subvolumes;
+    # these are also git-annex repositories
+    # and subvolumes backed up with btrbk
+    "v ${config.xdg.userDirs.documents} - - - - -"
+    "v ${config.xdg.userDirs.music} - - - - -"
+    "v ${config.xdg.userDirs.pictures} - - - - -"
+    "v ${config.xdg.userDirs.videos} - - - - -"
+
+    # create Steam recordings folder as a subvolume so it is not included in btrbk backups
+    "v ${config.xdg.userDirs.videos}/Recordings/steam - - - - -"
+  ];
+
   xdg = {
     enable = true;
     configFile = {
