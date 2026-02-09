442036734dec80426cc23166410abb20125b652a
municorn <municorn@musicaloft.com>
2026-02-09 08:27:45 -0700

    feat(home-assistant): add caddy configuration

--- server/home-assistant.nix
+++ server/home-assistant.nix
@@ -1,30 +1,42 @@
+{ config, ... }:
 {
   networking.firewall = {
     allowedTCPPorts = [ 4002 ];
     allowedUDPPorts = [ 4002 ];
   };
 
-  services.home-assistant = {
-    enable = true;
-    openFirewall = true;
-    config = {
-      default_config = { };
-      homeassistant = {
-        unit_system = "us_customary";
-        time_zone = "America/Boise";
-        name = "Sunni's Home";
-      };
-      scene = "!include scenes.yaml";
-      automation = "!include automations.yaml";
+  services = {
+    caddy = {
+      enable = true;
+      email = "caddy@musicaloft.com";
+
+      virtualHosts."hass.municorn.me".extraConfig = ''
+        reverse_proxy 127.0.0.1:${toString config.services.home-assistant.config.http.server_port}
+      '';
     };
-    configWritable = true;
-    extraComponents = [
-      "default_config"
-      "esphome"
-      "google_translate"
-      "govee_light_local"
-      "met"
-      "tplink"
-    ];
+
+    home-assistant = {
+      enable = true;
+      openFirewall = true;
+      config = {
+        default_config = { };
+        homeassistant = {
+          unit_system = "us_customary";
+          time_zone = "America/Boise";
+          name = "Sunni's Home";
+        };
+        scene = "!include scenes.yaml";
+        automation = "!include automations.yaml";
+      };
+      configWritable = true;
+      extraComponents = [
+        "default_config"
+        "esphome"
+        "google_translate"
+        "govee_light_local"
+        "met"
+        "tplink"
+      ];
+    };
   };
 }
