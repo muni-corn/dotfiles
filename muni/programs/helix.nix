62e628da61a59f473aac18b935dd6834f28e010f
municorn <municorn@musicaloft.com>
2026-02-10 00:05:43 -0700

    feat(helix): remove broken `zls` and re-enable mdformat

--- muni/programs/helix.nix
+++ muni/programs/helix.nix
@@ -19,19 +19,18 @@
       vscode-langservers-extracted
       vtsls
       wgsl-analyzer
-      zls
 
-      # (mdformat.withPlugins (
-      #   p: with p; [
-      #     mdformat-admon
-      #     mdformat-footnote
-      #     mdformat-frontmatter
-      #     mdformat-myst
-      #     mdformat-simple-breaks
-      #     mdformat-tables
-      #     mdformat-wikilink
-      #   ]
-      # ))
+      (mdformat.withPlugins (
+        p: with p; [
+          mdformat-admon
+          mdformat-footnote
+          mdformat-frontmatter
+          mdformat-myst
+          mdformat-simple-breaks
+          mdformat-tables
+          mdformat-wikilink
+        ]
+      ))
     ];
     defaultEditor = true;
     settings = {
