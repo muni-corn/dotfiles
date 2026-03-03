3e4cc6e443539795e90e9867e1d900c7a498b035
municorn <municorn@musicaloft.com>
2026-03-03 07:44:03 -0700

    fix(server): switch to proprietary nvidia drivers, since the open ones fail builds

--- server/hardware.nix
+++ server/hardware.nix
@@ -123,7 +123,7 @@
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     nvidia = {
       modesetting.enable = true;
-      open = true;
+      open = false;
       prime = {
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
