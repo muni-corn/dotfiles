2a27612e036ff7af676257c0e067cbeed45d6c0f
municorn <municorn@musicaloft.com>
2026-04-01 23:06:05 -0600

    chore(firewall): remove stupid sharteum ports
    bye, Shardeum! <3

--- firewall.nix
+++ firewall.nix
@@ -3,9 +3,6 @@
     # ports for ssh and minecraft should be opened by their corresponding
     # configurations
     allowedTCPPorts = [
-      10001
-      9001
-
       # for development
       8080
     ];
