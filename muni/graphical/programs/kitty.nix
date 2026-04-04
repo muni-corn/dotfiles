d4a3f44abcfbac14b6ba9bec80f1efe5c1a7d160
municorn <municorn@musicaloft.com>
2026-04-03 18:57:58 -0600

    refactor(kitty): remove nnn preview socket configuration
    - remove `listen_on` and `allow_remote_control` settings that were only used for nnn previews
    - simplify kitty configuration by removing unused remote control socket setup

--- muni/graphical/programs/kitty.nix
+++ muni/graphical/programs/kitty.nix
@@ -4,10 +4,6 @@
     settings = {
       shell_integration = "enabled";
 
-      # for nnn previews
-      listen_on = "unix:/tmp/kitty";
-      allow_remote_control = "socket-only";
-
       # theme font
       modify_font = "cell_height 115%";
 
