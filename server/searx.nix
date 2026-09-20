fa5708392615676f64a7166703d3328609bb4893
municorn <municorn@musicaloft.com>
2026-09-20 11:07:03 -0600

    fix(server/searx): bind to loopback

--- server/searx.nix
+++ server/searx.nix
@@ -18,7 +18,7 @@
         ];
         server = {
           port = 7327;
-          bind_address = "0.0.0.0";
+          bind_address = "127.0.0.1";
           secret_key = "@SEARX_SECRET_KEY@";
         };
       };
